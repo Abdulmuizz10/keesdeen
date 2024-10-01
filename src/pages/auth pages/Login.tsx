488538f906e956bfb99a9253645993ad81d03e90
Your Name <harrismuizz10@gmail.com>
2024-10-01 15:28:57 +0100

    updating login and signup pages

--- src/pages/auth pages/Login.tsx
+++ src/pages/auth pages/Login.tsx
@@ -1,3 +1,192 @@
+// "use client";
+
+// import React, { useContext, useState } from "react";
+// import { Button, Input, Label } from "@relume_io/relume-ui";
+// import type { ButtonProps } from "@relume_io/relume-ui";
+// import { BiLogoGoogle } from "react-icons/bi";
+// import { Link, useHistory } from "react-router-dom";
+// import { mainLogo } from "../../assets";
+// import { AuthContext } from "../../context/AuthContext/AuthContext";
+// import { Login } from "../../context/AuthContext/AuthApiCalls";
+
+// type ImageProps = {
+//   url?: string;
+//   src: string;
+//   alt?: string;
+// };
+
+// type Props = {
+//   logo: ImageProps;
+//   title: string;
+//   description: string;
+//   logInButton: ButtonProps;
+//   logInWithGoogleButton: ButtonProps;
+//   forgotPassword: {
+//     text: string;
+//     url: string;
+//   };
+//   image: ImageProps;
+//   signUpText: string;
+//   signUpLink: {
+//     text: string;
+//     url: string;
+//   };
+//   footerText: string;
+// };
+
+// export type Login7Props = React.ComponentPropsWithoutRef<"section"> &
+//   Partial<Props>;
+
+// export const Login7: React.FC = (props: Login7Props) => {
+//   const {
+//     logo,
+//     title,
+//     description,
+//     logInButton,
+//     logInWithGoogleButton,
+//     image,
+//     signUpText,
+//     signUpLink,
+//     footerText,
+//   } = {
+//     ...Login7Defaults,
+//     ...props,
+//   } as Props;
+
+//   const [email, setEmail] = useState<string>("");
+//   const [password, setPassword] = useState<string>("");
+//   const { dispatch } = useContext(AuthContext);
+
+//   const history = useHistory();
+
+//   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log({ email, password });
+//     Login({ email, password }, dispatch, history);
+//   };
+
+//   return (
+//     <section>
+//       <div className="relative grid min-h-screen grid-cols-1 items-stretch justify-center overflow-auto lg:grid-cols-2">
+//         <div className="absolute bottom-auto left-0 right-0 top-0 z-10 flex h-16 w-full items-center justify-center px-[5%] md:h-18 lg:justify-between">
+//           <a href={logo.url}>
+//             <img src={logo.src} alt={logo.alt} className="w-full h-[25px]" />
+//           </a>
+//         </div>
+//         <div className="relative mx-[5vw] flex items-center justify-center pb-16 pt-20 md:pb-20 md:pt-24 lg:py-20">
+//           <div className="container max-w-sm">
+//             <div className="container mb-6 max-w-lg text-center md:mb-8">
+//               <h1 className="mb-5 text-5xl font-bold md:mb-6 md:text-7xl lg:text-8xl bricolage-grotesque">
+//                 {title}
+//               </h1>
+//               <p className="md:text-md">{description}</p>
+//             </div>
+//             <form className="grid grid-cols-1 gap-6" onSubmit={handleSubmit}>
+//               <div className="grid w-full items-center">
+//                 <Label htmlFor="email" className="mb-2">
+//                   Email*
+//                 </Label>
+//                 <Input
+//                   type="email"
+//                   id="email"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                   required
+//                 />
+//               </div>
+//               <div className="grid w-full items-center">
+//                 <div className="flex items-start justify-between">
+//                   <Label htmlFor="password" className="mb-2">
+//                     Password*
+//                   </Label>
+//                   {/* <a href={forgotPassword.url} className="underline">
+//                     {forgotPassword.text}
+//                   </a> */}
+//                 </div>
+//                 <Input
+//                   type="password"
+//                   id="password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                   required
+//                 />
+//               </div>
+//               <div className="grid-col-1 grid gap-4">
+//                 <Button
+//                   variant={logInButton.variant}
+//                   size={logInButton.size}
+//                   iconLeft={logInButton.iconLeft}
+//                   iconRight={logInButton.iconRight}
+//                 >
+//                   {logInButton.title}
+//                 </Button>
+//                 <Button
+//                   variant={logInWithGoogleButton.variant}
+//                   size={logInWithGoogleButton.size}
+//                   iconLeft={logInWithGoogleButton.iconLeft}
+//                   iconRight={logInWithGoogleButton.iconRight}
+//                   className="gap-x-3"
+//                 >
+//                   {logInWithGoogleButton.title}
+//                 </Button>
+//               </div>
+//             </form>
+//             <div className="mt-5 inline-flex w-full items-center justify-center gap-x-1 text-center md:mt-6">
+//               <p>{signUpText}</p>
+//               <Link to={signUpLink.url} className="underline">
+//                 {signUpLink.text}
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="hidden bg-background-secondary lg:block">
+//           <img
+//             src={image.src}
+//             alt={image.alt}
+//             className="h-full w-full object-cover"
+//           />
+//         </div>
+//         <footer className="absolute bottom-0 left-0 right-0 top-auto flex h-16 w-full items-center justify-center pr-[5%] md:h-18 lg:justify-start lg:px-[5%]">
+//           <p className="text-sm">{footerText}</p>
+//         </footer>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export const Login7Defaults: Login7Props = {
+//   logo: {
+//     url: "/",
+//     src: mainLogo,
+//     alt: "Logo text",
+//   },
+//   title: "Log in",
+//   description: "Lorem ipsum dolor sit amet adipiscing elit.",
+//   logInButton: {
+//     title: "Log in",
+//   },
+//   logInWithGoogleButton: {
+//     variant: "secondary",
+//     title: "Log in with Google",
+//     iconLeft: <BiLogoGoogle className="size-6" />,
+//   },
+//   image: {
+//     src: "https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg",
+//     alt: "Relume placeholder image",
+//   },
+//   forgotPassword: {
+//     text: "Forgot your password?",
+
+//     url: "#",
+//   },
+//   signUpText: "Don't have an account?",
+//   signUpLink: {
+//     text: "Sign up",
+//     url: "/register/signUp",
+//   },
+//   footerText: "© 2024 Keesdeen",
+// };
+
 "use client";
 
 import React, { useContext, useState } from "react";
@@ -21,17 +210,12 @@ type Props = {
   description: string;
   logInButton: ButtonProps;
   logInWithGoogleButton: ButtonProps;
-  forgotPassword: {
-    text: string;
-    url: string;
-  };
   image: ImageProps;
   signUpText: string;
   signUpLink: {
     text: string;
     url: string;
   };
-  footerText: string;
 };
 
 export type Login7Props = React.ComponentPropsWithoutRef<"section"> &
@@ -47,7 +231,6 @@ export const Login7: React.FC = (props: Login7Props) => {
     image,
     signUpText,
     signUpLink,
-    footerText,
   } = {
     ...Login7Defaults,
     ...props,
@@ -66,7 +249,7 @@ export const Login7: React.FC = (props: Login7Props) => {
   };
 
   return (
-    <section>
+    <section className="bg-neutral-100">
       <div className="relative grid min-h-screen grid-cols-1 items-stretch justify-center overflow-auto lg:grid-cols-2">
         <div className="absolute bottom-auto left-0 right-0 top-0 z-10 flex h-16 w-full items-center justify-center px-[5%] md:h-18 lg:justify-between">
           <a href={logo.url}>
@@ -92,23 +275,20 @@ export const Login7: React.FC = (props: Login7Props) => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   required
+                  className="border-neutral-300 rounded"
                 />
               </div>
               <div className="grid w-full items-center">
-                <div className="flex items-start justify-between">
-                  <Label htmlFor="password" className="mb-2">
-                    Password*
-                  </Label>
-                  {/* <a href={forgotPassword.url} className="underline">
-                    {forgotPassword.text}
-                  </a> */}
-                </div>
+                <Label htmlFor="password" className="mb-2">
+                  Password*
+                </Label>
                 <Input
                   type="password"
                   id="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   required
+                  className="border-neutral-300 rounded"
                 />
               </div>
               <div className="grid-col-1 grid gap-4">
@@ -125,7 +305,7 @@ export const Login7: React.FC = (props: Login7Props) => {
                   size={logInWithGoogleButton.size}
                   iconLeft={logInWithGoogleButton.iconLeft}
                   iconRight={logInWithGoogleButton.iconRight}
-                  className="gap-x-3"
+                  className="bg-gray-300 hover:bg-gray-400 text-black rounded flex items-center gap-x-3"
                 >
                   {logInWithGoogleButton.title}
                 </Button>
@@ -133,7 +313,7 @@ export const Login7: React.FC = (props: Login7Props) => {
             </form>
             <div className="mt-5 inline-flex w-full items-center justify-center gap-x-1 text-center md:mt-6">
               <p>{signUpText}</p>
-              <Link to={signUpLink.url} className="underline">
+              <Link to={signUpLink.url} className="underline text-[#04BB6E]">
                 {signUpLink.text}
               </Link>
             </div>
@@ -146,9 +326,6 @@ export const Login7: React.FC = (props: Login7Props) => {
             className="h-full w-full object-cover"
           />
         </div>
-        <footer className="absolute bottom-0 left-0 right-0 top-auto flex h-16 w-full items-center justify-center pr-[5%] md:h-18 lg:justify-start lg:px-[5%]">
-          <p className="text-sm">{footerText}</p>
-        </footer>
       </div>
     </section>
   );
@@ -172,17 +349,11 @@ export const Login7Defaults: Login7Props = {
   },
   image: {
     src: "https://d22po4pjz3o32e.cloudfront.net/placeholder-image.svg",
-    alt: "Relume placeholder image",
-  },
-  forgotPassword: {
-    text: "Forgot your password?",
-
-    url: "#",
+    alt: "Placeholder image",
   },
   signUpText: "Don't have an account?",
   signUpLink: {
     text: "Sign up",
     url: "/register/signUp",
   },
-  footerText: "© 2024 Keesdeen",
 };
